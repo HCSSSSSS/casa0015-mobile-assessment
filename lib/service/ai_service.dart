a3f643c756986cfcfcf04700fcf87b50af184100
HCS <HCS20020708@gmail.com>
2026-03-16 05:16:35 +0800

    safer api

--- lib/service/ai_service.dart
+++ lib/service/ai_service.dart
@@ -1,11 +1,12 @@
 import 'dart:io';
 import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart'; // 引入读取保险箱的库
 
 class AIService {
+
   static Future<String?> analyzeFood(File imageFile) async {
-    // 从 .env 文件中动态读取 Key
+    // 安全做法：从 .env 文件中动态读取 Key
     final apiKey = dotenv.env['GEMINI_API_KEY'];
 
     if (apiKey == null || apiKey.isEmpty) {
@@ -14,16 +15,13 @@ class AIService {
     }
 
     try {
-      final model = GenerativeModel(
-        model: 'gemini-2.5-flash',
-        apiKey: apiKey,
-      );
+      final model = GenerativeModel(model: 'gemini-2.5-flash', apiKey: apiKey);
 
       final imageBytes = await imageFile.readAsBytes();
 
       final prompt = TextPart('''
         Analyze this image. If it contains food, identify it and estimate its nutritional value per serving.
-        You MUST respond ONLY with a valid JSON format exactly like this example, without any markdown formatting:
+        You MUST respond ONLY with a valid JSON format exactly like this example, without any markdown formatting like ```json:
         {
           "food_name": "Steak and Eggs",
           "calories": 828,
